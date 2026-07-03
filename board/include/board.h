52beb1a7fa71a00c15bff419cc18bb7f4c33eae7
voidoxin <225691883+voidoxin@users.noreply.github.com>
2026-07-03 09:56:05 +0100

    new architect(array bitboard)

--- board/include/board.h
+++ board/include/board.h
@@ -2,25 +2,34 @@
 #include <string>
 using namespace std;
 using BitBoard uint64_t;
-struct WhitePieces{
-        BitBoard king;
-        BitBoard queen;
-        BitBoard Pawns;
-        BitBoard knights;
-        BitBoard Bishops;
-        BitBoard Rooks;
-            void refresh_board();
-            void move(int old_square,int new_square);
-            // clculations
+extern BitBoard squareMask[64];
+void initSqMask();
+enum class pieces{
+        empty=0,
+        White_king=2,
+        White_queen=3,
+        White_rooks=4,
+        White_bishops=5,
+        White_knights=6,
+        White_pawns=7,
+
+        Black_king=8,
+        Black_queen=9,
+        Black_rooks=10,
+        Black_bishops=11,
+        Black_knights=12,
+        Black_pawns=13,        
 }
-struct BlackPieces{
-        BitBoard king;
-        BitBoard queen;
-        BitBoard Pawns;
-        BitBoard knights;
-        BitBoard Bishops;
-        BitBoard Rooks;
-            void refresh_board();
-            void move(int old_square,int new_square);
-            // clculations
-}
+class Board {
+    private :
+        BitBoard board_table[14];
+        int side to move=0;
+    public:
+        Board();
+        void board_init();
+        void add_piece();
+        void delete_piece(pieces piece,int square);
+        void move_piece(pieces piece,int from,int to);
+        void get_color();
+
+};
--- board/src/board.cpp
+++ board/src/board.cpp
@@ -1,35 +1,59 @@
 #include "../include/board.h"
-#include <iostream>
+#include <string>
 #include <cstdint>
-static void refresh(int king_square,int queen_square)
+using namespace std;
+void initSqMask ()
 {
-    // remove old board
-    king=0;
-    queen=0;
-    knights=0;
-    Rooks=0;
-    Bishops=0;
-    Pawns=0;
-    // new board init
-    king= 1ULL << king_square;
-    queen= 1ULL << queen_square;
-    //pawns
-    for(int pawn=8,pawn<=15,pawn++;)
+    for(int sq=0;sq<64;sq++)
     {
-        Pawns |= (1ULL << pawn);
+        squareMask[sq]= (1ULL << sq);
     }
-    rooks |= (1ULL << 0);
-    rooks |= (1ULL << 7);
-    knights |= (1ULL << 6);
-    knights |= (1ULL << 1);
-    Bishops |= (1ULL << 5);
-    Bishops |= (1ULL << 2);
 }
-void WhitePieces::refresh_board()
+void Board::add_piece(pieces piece,int square)
 {
-    refresh(4,3);
+    board_table[static_cast[int](piece)] |= squareMask[square];
 }
-void BlackPieces::refresh_board()
+void Board::init()
 {
-    refresh(3,4);
+    for (int i=0;i<=13;i++)
+    {
+        board[i]=0ULL;
+    }//delete evrything
+    add_piece(pieces::Black_king,60);
+    add_piece(pieces::Black_queen,59);
+    add_piece(pieces::Black_rooks,56);
+    add_piece(pieces::Black_rooks,63);
+    add_piece(pieces::Black_knights,57;
+    add_piece(pieces::Black_knights,62);
+    add_piece(pieces::Black_bishops,58);
+    add_piece(pieces::Black_bishops,61);
+    for(int pawn=48;pawn<=55;pawn++)
+    {
+        add_piece(pieces::Black_pawns,pawn);
+    }
+    add_piece(pieces::White_king,4);
+    add_piece(pieces::White_queen,3);
+    add_piece(pieces::White_rooks,0);
+    add_piece(pieces::White_rooks,7);
+    add_piece(pieces::White_knights,1);
+    add_piece(pieces::White_knights,6);
+    add_piece(pieces::White_bishops,2);
+    add_piece(pieces::White_bishops,5);
+    for(int pawn=8;pawn<=15;pawn++)
+    {
+        add_piece(pieces::White_pawns,pawn);
+    }
+    
+}
+void Board::delete_piece(pieces piece,int square)
+{
+    board_table[static_cast[int](piece)] ^= squareMask[square];
+}
+void Board::move_piece(pieces piece,int from,int to)
+{
+    
+}
+Board::Board(){
+    initSqMask();
+    init();
 }
